1e52e6e6ec63c30f65155b2b834f1127edf8a4bd
bhoward75051 <ben.howard75051@gmail.com>
2022-03-12 16:00:21 +0000

    Slight improvements

--- NQMPI/testProg.cpp
+++ NQMPI/testProg.cpp
@@ -1,13 +1,24 @@
 #include <mpi.h>
-#include <stdio.h>
+#include <iostream>
 int main(int argc, char** argv) {
     // Initialize the MPI environment
-    MPI_Init(NULL, NULL);
-    // Get the rank of the process
+    // Must run before all other MPI commands
+    MPI_Init(&argc, &argv);
+
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    //Get rank of the processdes
     int my_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
     // Print the message
-    printf("Hello World! My rank is %d\n", my_rank);
+    std::cout << "World Size: " << world_size << "  Rank: " << my_rank << std::endl;
+
     // Finalize the MPI environment.
     MPI_Finalize();
 }
+
+
+//mpiexec -n <number of processes> ./NQMPI.exe
