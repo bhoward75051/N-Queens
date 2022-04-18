7c315d354875fe645616bad404d6ef38891866f1
bhoward75051 <ben.howard75051@gmail.com>
2022-04-18 18:10:11 +0100

    Somers code in C++
    Looking to parallize it

---
+++ Jeff Somers implementation/somers.cpp
@@ -0,0 +1,105 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+typedef unsigned long ul;
+const int MINBOARDSIZE = 2;
+const int MAXBOARDSIZE = 18;
+
+ul numberOfSol = 0;
+
+void Nqueen(int boardSize) {
+    int nQueenBitRes[MAXBOARDSIZE]; //Results
+    int nQueenBitCol[MAXBOARDSIZE]; //Marks columns with queens
+    int nQueenPosDiag[MAXBOARDSIZE]; //marks positive diagonals with queens
+    int nQueenNegDiag[MAXBOARDSIZE]; //Marks negative diagonals with queens
+    int nStack[MAXBOARDSIZE + 2];
+    int* pnStack;
+
+    int numrows = 0;
+    unsigned int lsb; //Least significant bit
+    unsigned int bitfield; //bits which are a set mark possible positions for a queen
+    int i;
+    int odd = boardSize & 1; //0 if boardSize is even, 1 for odd
+    int boardMinus = boardSize - 1; 
+    int mask = (1 << boardSize) -1; //if board size is N, mask consists of N 1's
+
+    nStack[0] = -1; //set senital (end of stack)
+
+    for (i = 0; i < (1 + odd); i++) {
+        bitfield = 0;
+        if (i == 0) {
+            int half = boardSize>>1;
+            bitfield = (1 << half) -1;
+            pnStack = nStack + 1;
+
+            nQueenBitRes[0] = 0;
+            nQueenBitCol[0] = nQueenPosDiag[0] = nQueenNegDiag[0] = 0;
+        }
+        else {
+            bitfield = 1 << (boardSize >> 1);
+            numrows = 1;
+
+            nQueenBitRes[0] = bitfield;
+            nQueenBitCol[0] = nQueenPosDiag[0] = nQueenNegDiag[0] = 0;
+            nQueenBitCol[1] = bitfield;
+
+            nQueenNegDiag[1] = (bitfield >> 1);
+            nQueenPosDiag[1] = (bitfield << 1);
+            pnStack = nStack + 1;
+            *pnStack++ = 0; 
+            bitfield = (bitfield - 1) >> 1;
+        }
+
+        for (;;) {
+            lsb = -((signed)bitfield) & bitfield;
+            if (bitfield == 0) {
+                bitfield = *--pnStack;
+                if (pnStack == nStack) {
+                    break;
+                }
+                --numrows;
+                continue;
+            }
+            bitfield &= ~lsb;
+
+            nQueenBitRes[numrows] = lsb;
+            if (numrows < boardMinus) {
+                int n = numrows++;
+                nQueenBitCol[numrows] = nQueenBitCol[n] | lsb;
+                nQueenNegDiag[numrows] = (nQueenNegDiag[n] | lsb) >> 1;
+                nQueenPosDiag[numrows] = (nQueenPosDiag[n] | lsb) << 1;
+                *pnStack++ = bitfield;
+                bitfield = mask & ~(nQueenBitCol[numrows] | nQueenNegDiag[numrows] | nQueenPosDiag[numrows]);
+                continue;
+            }
+            else {
+                ++numberOfSol;
+                bitfield = *--pnStack;
+                --numrows;
+                continue;
+            }
+        }
+    }
+    numberOfSol *= 2;
+}
+
+int main(int argc, char** argv) {
+    int boardSize;
+
+    if (argc != 2) {
+        cout << "Not enough args" << endl;
+        return 0;
+    }
+
+    boardSize = atoi(argv[1]);
+    if (MINBOARDSIZE > boardSize || MAXBOARDSIZE < boardSize) {
+        cout << "Incorrect Board Size" << endl;
+        return 0;
+    }
+
+    Nqueen(boardSize); 
+
+    cout << numberOfSol << endl;;
+}
