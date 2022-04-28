bb6a57390c5a1145fde9cbed9f04155fcfe51967
bhoward75051 <ben.howard75051@gmail.com>
2022-04-28 14:27:46 +0100

    Update jobScript.sh

--- Parallel Backtracking/jobScript.sh
+++ Parallel Backtracking/jobScript.sh
@@ -11,15 +11,8 @@
 #Memory per process in MB
 #SBATCH --mem-per-cpu=2000
 
-#Number of parallel processes (Tasks)
-#SBATCH --ntasks=15
-
-#Tasks to run per node
-#SBATCH --tasks-per-node=4
-
 ###
-module load compiler/intel mpi/intel
 ./generateValues 15 1 "depthValues.txt"
 
 #run Intel MPI Benchmarks with mpirun - will automatically pick up Slurm parallel environment
-mpirun $MPI_HOME/intel64/bin/IMB-MPI1 15 1 "depthValues.txt"
+mpirun -n 4 ./autoTest 15 1 "depthValues.txt"
