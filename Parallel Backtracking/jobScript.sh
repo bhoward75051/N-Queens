cfa32df7e3f7940bfdbc8002c9c99a56ea08a7fe
bhoward75051 <ben.howard75051@gmail.com>
2022-04-28 14:41:26 +0100

    Update jobScript.sh

--- Parallel Backtracking/jobScript.sh
+++ Parallel Backtracking/jobScript.sh
@@ -4,20 +4,15 @@
 #SBATCH -p compute
 #SBATCH -o runout.%J
 #SBATCH -e runerr.%J
+#SBATCH -n 8
 
-#Job name
 #SBATCH --job-name=nq_bench
 
-#maximum job time in D-HH:MM
 #SBATCH --time=0-00:10
 
-#Number of parallel processes (Tasks)
-#SBATCH -n=8
-
 #SBATCH --mem-per-cpu=2000
-
-
 ###
+
 module load compiler/intel mpi/intel
 ./generateValues 15 1 "depthValues.txt"
 
