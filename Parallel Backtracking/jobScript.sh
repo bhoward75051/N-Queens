febfb9861a912b8594c2cd110828d6f14eaa1263
bhoward75051 <ben.howard75051@gmail.com>
2022-04-28 14:40:17 +0100

    Update jobScript.sh

--- Parallel Backtracking/jobScript.sh
+++ Parallel Backtracking/jobScript.sh
@@ -1,6 +1,9 @@
 #!/bin/bash --login
 
 ###
+#SBATCH -p compute
+#SBATCH -o runout.%J
+#SBATCH -e runerr.%J
 
 #Job name
 #SBATCH --job-name=nq_bench
@@ -8,17 +11,14 @@
 #maximum job time in D-HH:MM
 #SBATCH --time=0-00:10
 
-#Memory per process in MB
+#Number of parallel processes (Tasks)
+#SBATCH -n=8
+
 #SBATCH --mem-per-cpu=2000
 
-#Number of parallel processes (Tasks)
-#SBATCH --ntasks=8
-
-#Tasks to run per node
-#SBATCH --tasks-per-node=1
 
 ###
-module load mpi/mpich/3.2.1
+module load compiler/intel mpi/intel
 ./generateValues 15 1 "depthValues.txt"
 
 #run Intel MPI Benchmarks with mpirun - will automatically pick up Slurm parallel environment
