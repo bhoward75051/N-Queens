68f555ee6b0960f0f2fc560e9a39ca289b49b51c
bhoward75051 <ben.howard75051@gmail.com>
2022-04-29 21:58:52 +0100

    Update jobScript.sh

--- Parallel Backtracking/jobScript.sh
+++ Parallel Backtracking/jobScript.sh
@@ -19,4 +19,22 @@ module load mpi/mpich/3.2.1
 ./generateValues 15 2 "depthValues.txt"
 /usr/bin/time -f "%E %U %S" -a -o testResultsHPC.txt time mpiexec ./autoTest 15 2 "depthValues.txt"
 
+for N in {10..17}
+do
+    ./generateValues $N 5 "depthValues.txt"
+    #SBATCH -n 1
+    /usr/bin/time -f "%E %U %S" -a -o testResultsHPC.txt time mpiexec ./autoTest $N 5 "depthValues.txt"
+    #SBATCH -n 2
+    /usr/bin/time -f "%E %U %S" -a -o testResultsHPC.txt time mpiexec ./autoTest $N 5 "depthValues.txt"
+    #SBATCH -n 4
+    /usr/bin/time -f "%E %U %S" -a -o testResultsHPC.txt time mpiexec ./autoTest $N 5 "depthValues.txt"
+    #SBATCH -n 8
+    /usr/bin/time -f "%E %U %S" -a -o testResultsHPC.txt time mpiexec ./autoTest $N 5 "depthValues.txt"
+    #SBATCH -n 16
+    /usr/bin/time -f "%E %U %S" -a -o testResultsHPC.txt time mpiexec ./autoTest $N 5 "depthValues.txt"
+    #SBATCH -n 32
+    /usr/bin/time -f "%E %U %S" -a -o testResultsHPC.txt time mpiexec ./autoTest $N 5 "depthValues.txt"
+    #SBATCH -n 64
+    /usr/bin/time -f "%E %U %S" -a -o testResultsHPC.txt time mpiexec ./autoTest $N 5 "depthValues.txt"
+done
 
