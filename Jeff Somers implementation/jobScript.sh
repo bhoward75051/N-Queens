d01e8597e47dac7b2d30bb3a2852a6545b1aa57e
bhoward75051 <ben.howard75051@gmail.com>
2022-05-01 16:38:39 +0100

    Update jobScript.sh

--- Jeff Somers implementation/jobScript.sh
+++ Jeff Somers implementation/jobScript.sh
@@ -4,11 +4,11 @@
 #SBATCH -p compute
 #SBATCH -o runout.%J
 #SBATCH -e runerr.%J
-#SBATCH -n 64
+#SBATCH -n 32
 
 #SBATCH --job-name=nq_bench
 
-#SBATCH --time=0-01:00
+#SBATCH --time=0-02:00
 
 #SBATCH --mem-per-cpu=2000
 ###
@@ -16,7 +16,7 @@
 module purge
 module load mpi/mpich/3.2.1
 
-for N in {10..20}
+for N in {10..21}
 do
     ./generateArray $N 6 
     /usr/bin/time -f "%E %U %S" -a -o testResultsBitHPC.txt time mpiexec ./bitwiseParallel $N 6
