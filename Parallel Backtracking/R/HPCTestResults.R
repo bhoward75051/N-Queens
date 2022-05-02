0c9b532acde551dc1ec86d0b69d2774100fa5eb7
bhoward75051 <ben.howard75051@gmail.com>
2022-05-02 16:00:34 +0100

    More graphs

--- Parallel Backtracking/R/HPCTestResults.R
+++ Parallel Backtracking/R/HPCTestResults.R
@@ -14,18 +14,30 @@ lines(data54[, 1], data54[, 4], col='green')
 legend(7, 95, legend=c("1 cores", "2 cores", "4 cores"),
        col=c("red", "blue", "green"), lty=1:1, cex=1)
 
-data14 <- data[ which(data$n == 14 ), ]
+data14 <- data[ which(data$n == 13 ), ]
 data14 <- data14[ which(data14$depth > 0 ), ]
 data141 <- data14[ which(data14$processes == 1 ), ]
 data142 <- data14[ which(data14$processes == 2 ), ]
-data144 <- data14[ which(data14$processes == 4 ), ]
-data142
 
 plot(data141[, 2], data141[, 4], col='red', type = "l", main="Time to compute 14 Queens problem", xlab="Depth", ylab="Time (seconds)")
 lines(data142[, 2], data142[, 4], col='blue')
-lines(data144[, 2], data144[, 4], col='green')
-legend(7, 95, legend=c("1 cores", "2 cores", "4 cores"),
-       col=c("red", "blue", "green"), lty=1:1, cex=1)
+legend(2, 95, legend=c("1 cores", "2 cores"),
+       col=c("red", "blue"), lty=1:1, cex=1)
+
+data1 <- data[ which(data$processes == 2 ), ]
+data1 <- data1[ which(data1$n > 6 ), ]
+data10 <- data1[ which(data1$depth == 0 ), ]
+data11 <- data1[ which(data1$depth == 1 ), ]
+data12 <- data1[ which(data1$depth == 2 ), ]
+
+data10
+data11
+
+plot(data10[, 1], data10[, 4], col='red', type = "l", main="Time to compute 14 Queens problem", xlab="N", ylab="Time (seconds)")
+lines(data11[, 1], data11[, 4], col='blue')
+lines(data12[, 1], data12[, 4], col='green')
+legend(4, 95, legend=c("Depth 0", "Depth 1"),
+       col=c("red", "blue"), lty=1:1, cex=1)
 
 data12
 plot(data12_1[, 2], data12_1[, 4])
--- Parallel Backtracking/jobScript.sh
+++ Parallel Backtracking/jobScript.sh
@@ -4,7 +4,7 @@
 #SBATCH -p compute
 #SBATCH -o runout.%J
 #SBATCH -e runerr.%J
-#SBATCH -n 2
+#SBATCH -n 24
 
 #SBATCH --job-name=nq_bench
 
